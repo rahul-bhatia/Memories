a56029740060aa365e76d217ca894457daa0b89c
unknown <rahul.1517.bhatia@gmail.com>
2020-11-27 21:09:23 +0530

    server patch function added. need to follow on client part from now

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -21,3 +21,15 @@ export const createPosts = async (req,res) =>{
         res.status(409).json({messgae:error.message});
     }
 }
+
+export const updatePosts = async (req,res) =>{
+    const {id :_id} = req.params;
+    const post=req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('Not an Valid Id ')
+
+    //This new:true will return the updated version of data
+    const updatedPost=await PostMessage.findByIdAndUpdate(_id,post ,{new:true})
+    res.json(updatePosts)
+
+}
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import {getPosts , createPosts} from '../controller/posts.js'
+import {getPosts , createPosts,updatePosts} from '../controller/posts.js'
 
 const router=express.Router();
 
 
 router.get('/',getPosts);
 router.post('/',createPosts);
+router.patch('/:id',updatePosts);
 
 
 export default router;
