cedc26ef5103f409e1296e8f6a871765e97ef998
unknown <rahul.1517.bhatia@gmail.com>
2020-12-04 20:19:30 +0530

    Final commit before deployment

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import dotenv from 'dotenv';
 import cors from "cors";
 
 import postRoutes from './routes/post.js';
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
@@ -14,14 +15,13 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
 
 app.use('/posts',postRoutes); 
-const CONNECTION_URL = "mongodb+srv://test123:test123@cluster0.eudj9.mongodb.net/memories?retryWrites=true&w=majority";
 
 const PORT =process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then( () => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
 .catch((error)=>{
-    console.log(error.message);
+    console.log(error);
 
 });
 
